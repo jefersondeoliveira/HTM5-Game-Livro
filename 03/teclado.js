0a720feb5fd52eaace899a5d6b57399bd457a787
jefersondeoliveira <jefersondeoliveiralopes@gmail.com>
2015-06-26 12:47:12 -0300

    Atirando

--- 03/teclado.js
+++ 03/teclado.js
@@ -1,21 +1,40 @@
 // Códigos de teclas - aqui vão todos os que forem necessários
 var SETA_ESQUERDA = 37; 
 var SETA_DIREITA = 39;
+var ESPACO = 32;
 
 function Teclado(elemento){
 
 	this.elemento = elemento;
+	
 	//array de teclas pressionadas
 	this.pressionadas = [];
+
+	//array de teclas disparadas
+	this.disparadas = [];
+
+	//funções de disparo
+	this.funcoesDisparo = [];
+
 	//registrando o estado das telcas no array
 	var teclado = this;
 
 	elemento.addEventListener('keydown',function(evento){
-		teclado.pressionadas[evento.keyCode] = true;
+		var tecla = evento.keyCode;
+		teclado.pressionadas[tecla] = true;
+
+		//disparar somente se for o primeiro keydown da tecla
+		if(teclado.funcoesDisparo[tecla] && !teclado.disparadas[tecla]){
+			teclado.disparadas[tecla] = true;
+			teclado.funcoesDisparo[tecla]();
+		}
+
+
 	});
 
 	elemento.addEventListener('keyup',function(evento){
 		teclado.pressionadas[evento.keyCode] = false;
+		teclado.disparadas[evento.keyCode] = false;
 	});
 
 }
@@ -23,5 +42,8 @@ function Teclado(elemento){
 Teclado.prototype = {
 	pressionada: function(tecla){
 		return this.pressionadas[tecla];
+	},
+	disparou: function(tecla, callback){
+		this.funcoesDisparo[tecla] = callback;
 	}
 }
